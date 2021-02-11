3b43641371401b9bd2f8605ebd7382ba856e56e2
xpbsh <45247477+xpbsh@users.noreply.github.com>
2021-02-11 22:52:09 +0000

    New Update

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,12 @@
 import axios from "axios";
 import config from "../config.json";
 import Head from "next/head";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+
+library.add(fab, fas)
 
 class UserPage extends React.Component {
   render() {
@@ -9,7 +15,6 @@ class UserPage extends React.Component {
         className="container"
         style={{
           fontFamily: config.user.font,
-          background: config.user.background,
           overflow: 'auto',
           borderRadius: config.user.corners,
         }}
@@ -17,25 +22,54 @@ class UserPage extends React.Component {
         <Head>
           <title>{config.web.title}</title>
         </Head>
-        <div className="main padfix">
+        <div className="main background">
 
-          <div className="flex">
-          <h1 className="logo" style={{color: config.user.colorSecondary, textTransform: "uppercase", fontSize: config.logo.size}}> <span style={{color: config.user.color, fontSize: config.logo.size2, textTransform: "lowercase", marginRight: 15}}>{config.user.username}</span>{config.logo.portfolio}</h1>
-          </div>
-          <div className="herocont">
+          <div className="nav flex">
             <div>
-              <img className="avatar" style={{height: config.avatar.size }} src="https://avatars2.githubusercontent.com/u/45247477" />
+            <img className="logo" style={{height: config.avatar.size }} src={config.user.logo} />
+            </div>
+          </div>
+          <div className="herocont flex">
+            <div>
               <h1 className="title" style={{ color: config.user.color, fontSize: config.web.titleSize }}>{config.web.title}</h1>
               <p className="subtitle" style={{color: config.user.colorSecondary, fontSize: config.web.subtitleSize}}>{config.web.subtitle}</p>
-            </div>
-          </div>
-            <div className="showcasecont">
-              <img className="showcase" src="heroimage.png"/>
-     <a href={config.web.link}> 
+              <p>{config.web.desc}</p>
+              <a href={config.web.link}> 
       <button className="mainbutton" 
       style={{color: config.web.mainbutton.color, background: config.web.mainbutton.background 
-      }}><i class="fab fa-dribbble"></i> {config.web.mainbutton.text}</button></a>
+      }}><FontAwesomeIcon icon={["fab", "dribbble"]} /> {config.web.mainbutton.text}</button></a>
             </div>
+            <div>
+              <img className="avatar" src="avatar.png" style={{ maxWidth: config.avatar.size }}/>
+            </div>
+          </div>
+        </div>
+        <div className="main projects">
+            <div className="flex">
+              <div className="left-line">
+              </div>
+              <div>
+              <h1 className="projh1">Projects</h1>
+              </div>
+            </div>
+            <div>
+            <a href={config.web.link}> 
+      <button className="projbtn" 
+      style={{color: config.web.mainbutton.color, background: config.web.mainbutton.background 
+      }}><FontAwesomeIcon icon={["fas", "mug-hot"]} /> support me</button></a>
+            </div>
+        </div>
+        <div>
+        <div className="project">
+        <div className="flex-grid">
+            {config.projects.map((project, index) => (
+            <div className="col center card">
+            <h1>{project.name}</h1>
+            <p>{project.desc}</p>
+            </div>
+            ))}
+        </div>
+        </div>
         </div>
       </div>
     );
